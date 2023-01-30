60377d532b0f60a8da8cd2da3feca504b7f100f8
Leandro Bianco <122483178+lebianco@users.noreply.github.com>
2023-01-30 18:51:32 -0300

    menu

---
+++ script/script.js
@@ -0,0 +1,33 @@
+const nav = document.querySelector(".nav");
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".nav-list");
+
+function handleButtonClick(event) {
+    if (event.type === "touchstart") event.preventDefalut();
+    event.stopPropagation();
+    nav.classList.toggle("active");
+    handleClickOutside(menu, () => {
+        nav.classList.remove("active");
+    });
+}
+
+function handleClickOutside(targetElement, callBack) {
+    const html = document.documentElement;
+    function handleHTMLClick(event) {
+        if (!targetElement.contains(event.target)) {
+            targetElement.removeAttribute("data-target")
+            html.removeEventListener("click", handleHTMLClick);
+            html.removeEventListener("touchstart", handleHTMLClick);
+            callBack();
+        }
+    }
+    if (!targetElement.hasAttribute("data-target")) {
+        html.addEventListener("click", handleHTMLClick)
+        html.addEventListener("touchstart", handleHTMLClick)
+
+        targetElement.setAttribute("data-target", "")
+    }
+}
+
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
