c02f9de8524a2086a09dfa14b5ea56fcc6266265
Leandro Bianco <122483178+lebianco@users.noreply.github.com>
2023-01-31 17:41:39 -0300

    Menu corrigido

--- script/script.js
+++ script/script.js
@@ -3,30 +3,30 @@ const btnMenu = document.querySelector(".btn-menu");
 const menu = document.querySelector(".nav-list");
 
 function handleButtonClick(event) {
-    if (event.type === "touchstart") event.preventDefalut();
-    event.stopPropagation();
-    nav.classList.toggle("active");
-    handleClickOutside(menu, () => {
-        nav.classList.remove("active");
-    });
+	if (event.type === "touchstart") event.preventDefalut();
+	event.stopPropagation();
+	nav.classList.toggle("active");
+	handleClickOutside(menu, () => {
+		nav.classList.remove("active");
+	});
 }
 
 function handleClickOutside(targetElement, callBack) {
-    const html = document.documentElement;
-    function handleHTMLClick(event) {
-        if (!targetElement.contains(event.target)) {
-            targetElement.removeAttribute("data-target")
-            html.removeEventListener("click", handleHTMLClick);
-            html.removeEventListener("touchstart", handleHTMLClick);
-            callBack();
-        }
-    }
-    if (!targetElement.hasAttribute("data-target")) {
-        html.addEventListener("click", handleHTMLClick)
-        html.addEventListener("touchstart", handleHTMLClick)
+	const html = document.documentElement;
+	function handleHTMLClick(event) {
+		if (!targetElement.contains(event.target)) {
+			targetElement.removeAttribute("data-target")
+			html.removeEventListener("click", handleHTMLClick);
+			html.removeEventListener("touchstart", handleHTMLClick);
+			callBack();
+		}
+	}
+	if (!targetElement.hasAttribute("data-target")) {
+		html.addEventListener("click", handleHTMLClick)
+		html.addEventListener("touchstart", handleHTMLClick)
 
-        targetElement.setAttribute("data-target", "")
-    }
+		targetElement.setAttribute("data-target", "")
+	}
 }
 
 btnMenu.addEventListener("click", handleButtonClick);
